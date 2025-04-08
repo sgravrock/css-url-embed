8d10e64ebcff55d0e91ab2f00c7b07d5495e22aa
Steve Gravrock <sdg@panix.com>
2025-04-08 05:40:48 -0700

    Reformat
    * Indent with tabs
    * Break most long lines

--- tasks/css-url-embed.js
+++ tasks/css-url-embed.js
@@ -1,35 +1,35 @@
-module.exports = function(grunt) {
-  var {processFile} = require('../lib/css-url-embed');
+module.exports = function (grunt) {
+	var {processFile} = require('../lib/css-url-embed');
 
-  grunt.registerMultiTask('cssUrlEmbed', "Embed URLs as base64 strings inside your stylesheets", function() {
-    var async = this.async();
+	grunt.registerMultiTask('cssUrlEmbed', "Embed URLs as base64 strings inside your stylesheets", function () {
+		var async = this.async();
 
-    var options = this.options({
-      failOnMissingUrl: true,
-      inclusive: false,
-      useMimeTypeSniffing: true
-    });
+		var options = this.options({
+			failOnMissingUrl: true,
+			inclusive: false,
+			useMimeTypeSniffing: true
+		});
 
-    var existingFiles = this.files.filter(function(file) {
-      if (!grunt.file.exists(file.src[0])) {
-        return false;
-      }
+		var existingFiles = this.files.filter(function (file) {
+			if (!grunt.file.exists(file.src[0])) {
+				return false;
+			}
 
-      return true;
-    });
+			return true;
+		});
 
-    var leftToProcess = existingFiles.length;
+		var leftToProcess = existingFiles.length;
 
-    if (leftToProcess === 0) {
-      async();
-    }
+		if (leftToProcess === 0) {
+			async();
+		}
 
-    existingFiles.forEach(function(file) {
-      processFile(grunt, file.src[0], file.dest, options, function() {
-        if (--leftToProcess === 0) {
-          async();
-        }
-      });
-    });
-  });
+		existingFiles.forEach(function (file) {
+			processFile(grunt, file.src[0], file.dest, options, function () {
+				if (--leftToProcess === 0) {
+					async();
+				}
+			});
+		});
+	});
 };
